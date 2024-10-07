6e42261cf7b7d8d67c087f6c45d4cbf5144028a1
Egore000 <egor.pakhomov.2002@gmail.com>
2024-10-07 13:38:09 +0800

    registration views

--- app/account/forms.py
+++ app/account/forms.py
@@ -1,3 +1,29 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+from django.utils.translation import gettext_lazy as _
+
+
+class UserRegistrationForm(forms.ModelForm):
+    """Форма создания аккаунта пользователя"""
+    
+    password = forms.CharField(
+        label=_('Пароль'),
+        widget=forms.PasswordInput,
+    )
+    password2 = forms.CharField(
+        label=_('Повторите пароль'),
+        widget=forms.PasswordInput,
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        password, password2 = self.cleaned_data['password'], self.cleaned_data['password2']
+
+        if password != password2:
+            raise forms.ValidationError(_('Пароли не совпадают!'))
+        return password2
+    
--- app/account/views.py
+++ app/account/views.py
@@ -1,14 +1,30 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy 
-from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import CreateView
+from django.http import HttpResponse, HttpRequest 
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 from . import forms
 
 
-# class SignUpView(CreateView):
-#     """Регистрация пользователя"""
+def register(request: HttpRequest) -> HttpResponse:
+    """Регистрация пользователя"""
 
-#     template_name = "registration/registration.html"
-#     form_class = forms.UserCreationForm
-#     success_url = reverse_lazy("account:login")
+    if request.method == 'POST':
+        form = forms.UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            user: User = form.save(commit=False)
+
+            user.set_password(
+                form.cleaned_data.get('password')
+            )
+
+            return render(request, 
+                          'account/register_done.html',
+                          context={'user': user}
+            )
+    else:
+        form = forms.UserRegistrationForm()
+    
+    return render(request, 
+                  'account/register.html',
+                  context={'form': form})
